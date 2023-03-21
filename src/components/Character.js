a44e43ad594d71c36280c892385d9e9a4eef3d37
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-03-21 18:06:59 +0100

    add eachCharacter

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,17 +1,17 @@
-function Character() {
+function Character({ eachCharacter }) {
   return (
     <li className="li">
       <img
         className="image__character"
-        src="https://ik.imagekit.io/hpapi/harry.jpg"
+        src={eachCharacter.image}
         alt="Foto del personaje"
         title="Foto del personaje"
       ></img>
-      <label>Nombre</label>
-      <label>Species</label>
-      <label>Gender</label>
-      <label>EyeColor</label>
-      <label>ancestry</label>
+      <h3>{eachCharacter.name}</h3>
+      <h3>{eachCharacter.species}</h3>
+      <h3>{eachCharacter.gender}</h3>
+      <h3>{eachCharacter.eyeColour}</h3>
+      <h3>{eachCharacter.ancestry}</h3>
     </li>
   );
 }
