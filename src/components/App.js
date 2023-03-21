c9f062f344f8fab17331b3be2e9de7461dda10ab
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-03-21 15:20:02 +0100

    add starter

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+/* SECCIÓN DE IMPORT */
+import "...styles/App.scss";
+// - Imágenes
+
+/* SECCIÓN DEL COMPONENTE */
+function App() {
+  /* VARIABLES ESTADO (DATOS) */
+
+  /* EFECTOS (día 5) */
+
+  /* FUNCIONES HANDLER */
+
+  /* FUNCIONES Y VARIABLES AUXILIARES PARA PINTAR EL HTML */
+
+  /* HTML */
+  return <div className="App">{/* Aquí va el HTML */}</div>;
+}
+
+/* PROP-TYPES */
+
+/* EXPORT DEL COMPONENTE */
+export default App;
