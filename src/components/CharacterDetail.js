62ae6e5dbda8adfe961fe1417e2a6531f4ce0890
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-06-01 18:21:40 +0200

    add responsive

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -4,28 +4,31 @@ import "../styles/components/CharacterDetail.scss";
 function CharacterDetail({ characterFind }) {
   return (
     <>
-      <section className="characterSection">
-        <div className="return">
-          <Link className="link" to="/list">
-            GO BACK
-            <i class="fa-sharp fa-solid fa-wand-sparkles"></i>
-          </Link>
-        </div>
-        <section className="character">
-          <article className="character__DetailItem">
-            <img
-              className="character__DetailItem-img"
-              src={characterFind.image}
-              alt="characterimage"
-              title="character image"
-            />
-            <section className="charInfo">
-              <h3 className="charInfo-h2">Name: {characterFind.name}</h3>
-              <h3 className="charInfo-h3">Specie: {characterFind.species}</h3>
-              <h3 className="charInfo-h3">House: {characterFind.house}</h3>
-              <h3 className="charInfo-h3">Gender: {characterFind.gender}</h3>
-            </section>
-          </article>
+      <section className="container">
+        <section className="container__characterSection">
+          <div className="container__characterSection-back">
+            <Link to="/list">
+              <button className="button">
+                GO BACK <i class="fa-sharp fa-solid fa-wand-sparkles"></i>
+              </button>
+            </Link>
+          </div>
+          <section className="container__character">
+            <article className="character__DetailItem">
+              <img
+                className="character__DetailItem-img"
+                src={characterFind.image}
+                alt="characterimage"
+                title="character image"
+              />
+              <section className="charInfo">
+                <h3 className="charInfo-h2">Name: {characterFind.name}</h3>
+                <h3 className="charInfo-h3">Specie: {characterFind.species}</h3>
+                <h3 className="charInfo-h3">House: {characterFind.house}</h3>
+                <h3 className="charInfo-h3">Gender: {characterFind.gender}</h3>
+              </section>
+            </article>
+          </section>
         </section>
       </section>
     </>
